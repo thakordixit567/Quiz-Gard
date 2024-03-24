af6a86efe786d12c2684b8abd59076775446029b
thakordixit567 <103193972+thakordixit567@users.noreply.github.com>
2024-03-24 14:20:21 +0530

    UPDATE!

---
+++ quiz-gard/src/Components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import logo from '/logo.png';
+const Navbar = () => {
+  return (
+    <header className="h-16 shadow-sm">
+      <nav>
+        <a href="/">
+          <img src={logo} alt="logo"/>
+          {/* Nav Items */}
+          <div>
+              <ul>
+              <li><a href="">How it's works ?</a></li>
+              <li><a href="">Fetures</a></li>
+              <li><a href="">About Us</a></li>
+              </ul>
+              <button className="font-medium px-5 py-1 border border-primary rounded">Log In</button>
+          </div>
+        </a>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
