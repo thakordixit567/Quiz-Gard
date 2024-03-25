f9aae153d339e202b07066601309ef64846b3d89
thakordixit567 <103193972+thakordixit567@users.noreply.github.com>
2024-03-25 15:40:08 +0530

    UPDATE!

--- quiz-gard/src/Components/Navbar.jsx
+++ quiz-gard/src/Components/Navbar.jsx
@@ -1,24 +1,26 @@
-import React from "react";
-import logo from '/logo.png';
+
 const Navbar = () => {
   return (
-    <header className="h-16 shadow-sm">
-      <nav className='flex'>
-        <a href="/">
-          <img src={logo} alt="logo"/>
-          {/* Nav Items */}
-          <div>
-              <ul>
-              <li><a href="">How it's works ?</a></li>
-              <li><a href="">Fetures</a></li>
-              <li><a href="">About Us</a></li>
-              </ul>
-              <button className="font-medium px-5 py-1 border border-primary rounded">Log In</button>
-          </div>
-        </a>
-      </nav>
-    </header>
-  );
-};
+    <header className="h-16 flex items-center shadow-sm">
+    <nav className="flex justify-between items-center w-9/12 mx-auto">
+      <a href="/" className="text-zinc-800 font-bold uppercase">
+        <img src="/images/logo.png" alt="logo" />
+      </a>
+      <div className="space-x-5 flex items-center">
+        <ul className="space-x-5 sm:flex hidden">
+          <li className="hover:text-yellow-500"><a href="/">How it works?</a></li>
+          <li className="hover:text-yellow-500"><a href="/">Features</a></li>
+          <li className="hover:text-yellow-500"><a href="/">About us</a></li>
+        </ul>
+        <button
+          className="font-medium px-5 py-1 border border-[#FCC822] rounded text-[#FCC822]"
+        >
+          Login
+        </button>
+      </div>
+    </nav>
+  </header>
+  )
+}
 
-export default Navbar;
+export default Navbar
